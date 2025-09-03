ba63c770d87493a32dd3efcd7557b681fb2bd372
ChrisSong1994 <18557511748@163.com>
2025-09-03 11:04:50 +0800

    feat: try file

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,30 @@ if (!STATIC_DIR) {
   throw new Error("STATIC_DIR environment variable is not set");
 }
 
-app.use(serveStatic(STATIC_DIR, { index: ["index.html", "index.htm"] }));
+app.use(
+  serveStatic(STATIC_DIR, {
+    index: ["index.html", "index.htm"],
+  })
+);
+
+// 相当于 Nginx 的 try_files $uri $uri/ /index.html;
+app.get(/(.*)/, (req, res) => {
+  const indexPath = path.join(STATIC_DIR, "index.html");
+
+  // 检查 index.html 是否存在
+  res.sendFile(indexPath, (err) => {
+    if (err) {
+      // 如果 index.html 也不存在，返回 404
+      res.status(404).send("Not Found: index.html missing in static directory");
+    }
+  });
+});
+
+// try_file
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(STATIC_DIR, "index.html"));
+// });
 
 app.listen(3080, () => {
-    console.log(`Static server running at http://localhost:3080`);
+  console.log(`Static server running at http://localhost:3080`);
 });
